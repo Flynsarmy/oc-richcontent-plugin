f84043bce081c58dcb8b8632a1868dc2393bae39
flynsarmy <flynsarmy@gmail.com>
2016-05-19 20:53:45 +1000

    first commit

---
+++ Plugin.php
@@ -0,0 +1,51 @@
+<?php namespace Flynsarmy\RichContent;
+
+use Event;
+use Backend;
+use System\Classes\PluginBase;
+use Backend\Widgets\Form;
+use Cms\Classes\Content;
+
+/**
+ * RichContent Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'RichContent',
+            'description' => 'Displays CMS Content markup as a richeditor if the filename ends in ".rich.htm"',
+            'author'      => 'Flynsarmy',
+            'icon'        => 'icon-file-text-o'
+        ];
+    }
+
+    public function boot()
+    {
+        Event::listen('backend.form.extendFields', function(Form $form) {
+            // Only modify CMS - Content forms
+            if ( !is_object($form->model) || !$form->model instanceof Content)
+                return;
+
+            // Make sure our target field exists
+            $field = $form->getField('markup');
+            if ( !$field )
+                return;
+
+            // Only modify existing content fields with filename ending in '.rich.htm'
+            $fileName = $form->model->fileName;
+            if ( substr($fileName, -9) != '.rich.htm' )
+                return;
+
+            // Convert to a richeditor
+            $field->config['type'] = $field->config['widget'] = 'richeditor';
+        });
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Rich Content
+
+This October CMS plugin conditionally converts the markup field in CMS - Content area to a richeditor field. This is useful, for example, when you have a CMS - Page with dynamic content on it and you just want to insert a bit of WYSIWYG text easily.
+
+## Usage
+
+* Create a CMS - Content with a filename `test.rich.htm`
+* Hit save
+* Close and re-open `test.rich.htm`
+
+## Licence
+
+The Rich Content plugin is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT).
