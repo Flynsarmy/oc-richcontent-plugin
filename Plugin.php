8b89682f27bb92923838d8943be2a99f860eceff
Flynsarmy <flynsarmy@gmail.com>
2020-01-31 06:10:55 +0000

    Apply fixes from StyleCI

--- Plugin.php
+++ Plugin.php
@@ -1,17 +1,17 @@
-<?php namespace Flynsarmy\RichContent;
+<?php
 
-use Event;
-use Backend;
-use System\Classes\PluginBase;
+namespace Flynsarmy\RichContent;
+
 use Backend\Widgets\Form;
 use Cms\Classes\Content;
+use Event;
+use System\Classes\PluginBase;
 
 /**
- * RichContent Plugin Information File
+ * RichContent Plugin Information File.
  */
 class Plugin extends PluginBase
 {
-
     /**
      * Returns information about this plugin.
      *
@@ -23,26 +23,29 @@ class Plugin extends PluginBase
             'name'        => 'RichContent',
             'description' => 'Displays CMS Content markup as a richeditor if the filename ends in ".rich.htm"',
             'author'      => 'Flynsarmy',
-            'icon'        => 'icon-file-text-o'
+            'icon'        => 'icon-file-text-o',
         ];
     }
 
     public function boot()
     {
-        Event::listen('backend.form.extendFields', function(Form $form) {
+        Event::listen('backend.form.extendFields', function (Form $form) {
             // Only modify CMS - Content forms
-            if ( !is_object($form->model) || !$form->model instanceof Content)
+            if (!is_object($form->model) || !$form->model instanceof Content) {
                 return;
+            }
 
             // Make sure our target field exists
             $field = $form->getField('markup');
-            if ( !$field )
+            if (!$field) {
                 return;
+            }
 
             // Only modify existing content fields with filename ending in '.rich.htm'
             $fileName = $form->model->fileName;
-            if ( substr($fileName, -9) != '.rich.htm' )
+            if (substr($fileName, -9) != '.rich.htm') {
                 return;
+            }
 
             // Convert to a richeditor
             $field->config['type'] = $field->config['widget'] = 'richeditor';
